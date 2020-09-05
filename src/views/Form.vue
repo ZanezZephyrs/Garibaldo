ad652c636589aca40a6bcdc2123264fbb7753722
ZanezZephyrs <thalesrogerio@gmail.com>
2020-09-05 18:30:15 -0300

    view do nathan

---
+++ src/views/Form.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+  <p>
+    <label> Digite o Seu nome:</label>
+    <input placeholder="Nome do Criador" type="text" name="criador"  v-model="host" />
+  </p>
+  <p>
+    <label> Digite o nome da Reunião:</label>
+    <textarea placeholder="Insira o nome da Reunião" name="nomereuniao"  v-model="reuniao"></textarea>
+  </p>
+  <button v-on:click="CriarReuniao" type="submit">Criar</button>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        host: '',
+        reuniao: ''
+      }
+    },
+    methods: {
+      CriarReuniao() {
+        if (this.reuniao.trim() === '') {
+          return;
+        }
+        this.host= this.host.trim()=== '' ? 'Anônimo' : this.host;
+        this.$router.push('/about')
+      }
+    }
+  }
+</script>
