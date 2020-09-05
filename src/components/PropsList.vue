86e88090353c7d5d215800f4b9c1bd2d642dae66
GiKerche <45881742+GiKerche@users.noreply.github.com>
2020-09-05 18:17:01 -0300

    Add files via upload

---
+++ src/components/PropsList.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="content">
+        <br>
+        <span> Seja bem-vinda(o) {{user.name}}</span>
+        <ul>
+            <li v-for="nome in nomes"> {{nome}} </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+    span{
+        color:red
+    }
+
+    .content{
+        max-width:300px;
+    }
+
+    h3,h4{
+        margin:0;
+    }
+
+</style>
+
+<script>
+    export default{
+        name:'PropsList',
+        // Propriedades que esperamos receber
+        props:{
+            nomes:{
+                type:Array,
+                required:true
+            },
+            user:{
+                type:Object,
+                required:true
+            }
+        },
+        data(){
+            return {}
+        }
+    }
+</script>
+
+
+
+
+
+
