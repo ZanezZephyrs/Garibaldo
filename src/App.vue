56ba7020c6eb2bc4aa461f7c31e9c0cf1499b202
GiKerche <45881742+GiKerche@users.noreply.github.com>
2020-09-05 22:08:12 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
 
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>|
-      <router-link to="/votacao"> votacao</router-link>|
-      <router-link to="/ordemdefala"> Ordem de Fala</router-link>|
-      <router-link to="/Form"> Form</router-link>|
-
-
+      <ul>
+          <li><router-link to="/">Home</router-link></li>     |  
+          <li><router-link to="/about">About</router-link></li>     |  
+          <li><router-link to="/votacao">votacao</router-link></li>     |  
+          <li> <router-link to="/add_pergunta">Criar pergunta</router-link></li>     |  
+          <li> <router-link to="/ordemdefala">Ordem de Fala</router-link></li>     |  
+          <li><router-link to="/Form">Form</router-link></li>
+      </ul>
     </div>
     <router-view/>
   </div>
@@ -16,24 +17,45 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
+body {
+  background: white;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #ed7efa;
+}
+
+#nav ul {
+    padding:10px;
+    margin:0px;
+    background-color:#fff2e9;
+    list-style:none;
+}
+
+#nav ul li { display: inline; }
+
+#nav ul li router-link {
+    padding: 20px 50px;
+    display: inline-block;
+
+    /* visual do link */
+    background-color:#a098f6;
+    color: #333;
+    text-decoration: none;
+    border-bottom:3px solid #EDEDED;
+}
+
+#nav ul li router-link:hover {
+    background-color:#fff2e9;
+    color: #a098f6;
+    border-bottom:10px solid #EA0000;
 }
 </style>
 
