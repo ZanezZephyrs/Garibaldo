d5be8194b991d928f6f6ba7072dfac3873805efc
GiKerche <45881742+GiKerche@users.noreply.github.com>
2020-09-06 11:25:19 -0300

    Add files via upload

---
+++ src/views/Fim.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="logo" id = "imagem" src="../assets/fim.gif">
+  </div>
+</template>
+<style>
+  body {
+    background: white;
+  }
+  
+  .home{
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  #imagem {
+      width: 85%;
+  }
+</style>
