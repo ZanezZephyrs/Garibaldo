6fdb284a2eb3d0c3859ae2801091abbbd0ef1de3
GiKerche <45881742+GiKerche@users.noreply.github.com>
2020-09-05 22:06:59 -0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="logo" id = "imagem" src="../assets/democracia.png">
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+<style>
+  body {
+    background: white;
   }
-}
-</script>
+  
+  .home{
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  #imagem {
+      width: 85%;
+  }
+</style>
